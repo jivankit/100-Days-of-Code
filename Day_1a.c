15ea1720c49574a957ebd6a45edd0222af292595
Jivankit <jivankit@gmail.com>
2025-09-02 09:19:56 +0530

    Add files via upload

---
+++ Day_1a.c
@@ -0,0 +1,18 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+
+int main() {
+    int n1, n2, sum=0;
+    
+    printf("Enter first number: ");
+    scanf("%d", &n1);
+    printf("Enter second number: ");
+    scanf("%d", &n2);
+    
+    sum = n1 + n2;
+    
+    printf("Sum = %d\n", sum);
+    
+    return 0;
+}
